7810af2ca601901b9ecb92d7441e627bb0266610
David Williams <willow316@hotmail.com>
2021-08-14 15:23:09 +0800

    week 5 portfolio with updates and menu

--- portfolio/week5/UserAccounts.sh
+++ portfolio/week5/UserAccounts.sh
@@ -1,29 +1,105 @@
-echo "Password File:"
+#!/bin/bash
 
-# Prints the titles in blue, it makes the second argument colour black with no spacing so it does not show, bin/bash filters out acounts with only bin/bash in the shell collum
+# Define variables
+#LSB=/usr/bin/lsb_release
 
-awk 'BEGIN {
-      
-    FS=":"; 
-      
-    print "_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________"; 
-      
-   
-    printf("| \033[34m%-20s\033[0m | \033[34m%-16s\033[0m | \033[34m%-10s\033[0m | \033[34m%-50s\033[0m | \033[34m%-35s\033[0m | \033[34m%-35s\033[0m |\n", "Username            ", 
-    "     UserID        ", "  Group ID    ", "   Function                   ","                                Home                           ","       Shell                                                  ");
+# Purpose: Display pause prompt
+# $1-> Message (optional)
+function pause(){
+	local message="$@"
+	[ -z $message ] && message="Press [Enter] key to continue..."
+	read -p "$message" readEnterKey
 }
 
-/bin\/bash/{
 
-printf("| \033[32m%-20s\033[0m\033[30m%-0s\033[0m| \033[36m%-16s\033[0m    |   \033[35m%-10s\033[0m   | \033[31m%-50s\033[0m | \033[31m%-35s\033[0m                             |\033[33m%-35s\033[0m                             |\n", $1, $2, $3, $4, $5, $6, $7);
+# Purpose  - Display a menu on screen
+function show_menu(){
+    tput setaf 2;
+	echo "---------------------------"
+	echo "Date and Time : " | date
+	tput setaf 7;
+    echo "---------------------------"
+    echo "   Main Menu"
+    echo "---------------------------"
+	echo "1. Show All Users"
+	echo "2. Filter /bin/bash"
+	echo "3. Filter /bin/false"
+	echo "4. Filter /usr/sbin/nologin"
+	echo "5. Filter /bin.sync"
+	echo "6. exit"
 
 }
 
-END {
+# Purpose - Display header message
+# $1 - message
+function write_header(){
+	local h="$@"
+	echo "---------------------------------------------------------------"
+	echo "     ${h}"
+	echo "---------------------------------------------------------------"
+}
 
-    print("_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+function main(){
+	./UserAccountsMain.sh
+	#pause "Press [Enter] key to continue..."
+	pause
+}
 
-}' passwd.txt
+# BIN BASH FILTER
+function binbash(){
+	./UserAccountsBinBash.sh
+	#pause "Press [Enter] key to continue..."
+	pause
+}
+
+# Purpose binfalse filter- 
+function binfalse(){
+	
+	./UserAccountsFALSE.sh
+	pause
+}
+
+# Purpose binsync filter- 
+function sync(){
+	./UserAccountsbinSYNC.sh
+			pause 
+}
+
+# Purpose user/sbin/nologin
+function sbinnologin(){
+	./UserAccountsSBINNOLOGIN.sh
+			pause 
+}
 
 
-exit 0
+# Purpose - Get input via the keyboard and make a decision using case..esac 
+function read_input(){
+	local c
+	tput setaf 2;
+	read -p "Enter your choice [ 1 - 6 ] " c
+		tput setaf 7;
+		case $c in
+		1)	main ;;
+		2)	binbash ;;
+		3)	binfalse ;;
+		4)  sbinnologin ;;
+		5)  sync ;;
+		6)	echo "Bye!"; exit 0 ;;
+		*)	
+			echo "Please select between 1 to 6 choice only."
+			tput setaf 6;
+			pause
+	esac
+	tput setaf 4;
+}
+
+# ignore CTRL+C, CTRL+Z and quit singles using the trap
+trap '' SIGINT SIGQUIT SIGTSTP
+
+# main logic
+while true
+do
+	clear
+show_menu
+read_input
+done
